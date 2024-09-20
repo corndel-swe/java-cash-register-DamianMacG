457e7f5f210572f348f1df5acedb6ca49c168772
Damian <damiangildea12@gmail.com>
2024-09-20 12:00:25 +0100

    refactor 2a

--- src/main/java/com/corndel/cashregister/exercises/Exercise2a.java
+++ src/main/java/com/corndel/cashregister/exercises/Exercise2a.java
@@ -8,14 +8,8 @@ public class Exercise2a {
    * Given a list of recipes, return the number of vegetarian recipes in the list.
    */
   public static int countVeggies(List<Recipe> recipes) {
-    int count = 0;
-
-    for (int i = 0; i < recipes.size(); i++) {
-      Recipe recipe = recipes.get(i);
-      if (recipe.isVegetarian()) {
-        count++;
-      }
-    }
-    return count;
+    return (int) recipes.stream()
+            .filter(Recipe::isVegetarian)
+            .count();
   }
 }
