f55e73e64a3809b1c9e55d3d8295c1f8fe2e1cdb
Damian <damiangildea12@gmail.com>
2024-09-20 11:12:01 +0100

    3a completed

--- src/main/java/com/corndel/cashregister/exercises/Exercise3a.java
+++ src/main/java/com/corndel/cashregister/exercises/Exercise3a.java
@@ -3,15 +3,20 @@ package com.corndel.cashregister.exercises;
 import com.corndel.cashregister.exercises.models.Recipe;
 import com.corndel.cashregister.exercises.models.RecipeBook;
 
+import java.util.List;
+
 public class Exercise3a {
   /**
    * Given a recipeBook containing recipes, returns the title of the last recipe
    * in the array.
-   * 
+   * <p>
    * Return null if there are no recipes.
    */
   public static Recipe lastRecipe(RecipeBook recipeBook) {
-    // TODO
-    return null;
+    if (recipeBook.getRecipes().isEmpty()) {
+      return null;
+    }
+    List<Recipe> recipes = recipeBook.getRecipes();
+    return recipes.getLast();
   }
 }
